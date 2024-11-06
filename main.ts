7969316166c728a5be99f909a9fba7aa14811de2
nathan-haimanot <nathan.haimanot@mths.ca>
2024-11-06 18:12:02 -0500

    made light go around micro bit perimeter

--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Nathan Haimanot
+ * Created on: Oct 2024
+ * This program moves a light across the screen perimeter using nested loops
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let counter = 0
+let spritemove = 0
+
+// happy
+basic.showIcon(IconNames.Happy)
+
+// on button A edge moving
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    counter = 0
+    sprite = game.createSprite(0, 0)
+    basic.pause(100)
+
+    // setting up the movement
+    while (counter < 4) {
+        spritemove = 0
+        while (spritemove < 4) {
+            sprite.move(1)
+            basic.pause(500)
+            spritemove++
+        }
+        sprite.turn(Direction.Right, 90)
+        counter++
+    }
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
